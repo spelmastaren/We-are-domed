1181aa559b9125e0a640154d435e423a5e0f5e57
spelmastaren <antonstegman@gmail.com>
2026-02-11 10:19:45 +0100

    Imported pygame and websockets packeges and instald in .venv

--- client/.venv/bin/activate.fish
+++ client/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/anton/Skrivbord/Programering 2/We are doomed/.venv'
+set -gx VIRTUAL_ENV /home/anton/Skrivbord/Programering-2/We-are-doomed/client/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
