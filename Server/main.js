df7bdbdbadaa52d84c2fd50da82556fe91b59604
spelmastaren <antonstegman@gmail.com>
2026-02-11 09:06:51 +0100

    Added player, ennemy, and lobby classes

--- Server/main.js
+++ Server/main.js
@@ -2,10 +2,46 @@ console.log("Server initialized");
 const ws = require("ws");
 const wss = new ws.Server({ port: 8080 });
 
+let lobys = new map();
+
+class lobby {
+    constructor(name) {
+        this.name = name;
+        this.map = randomizemap();
+        this.players = [];
+    }
+}
 
 function handelemessage(message) {
     console.log("Received message:", message);
 };
+
+function randomizemap() {
+    const map = [];
+    for (let i = 0; i < 10; i++) {
+        const row = [];
+        for (let j = 0; j < 10; j++) {
+            row.push(Math.random() < 0.4 ? 1 : 0); // 20% chance of being a wall
+        }
+        map.push(row);
+    }
+    return map;
+}
+
+class player {
+    constructor(name) {
+        this.Username = name;
+        this.position = { x: 0, y: 0 };
+        this.lobby = null;
+
+    }
+}
+
+class enemy {
+    constructor() {
+        this.position = { x: 0, y: 0 };
+    }
+}
 
 
 
