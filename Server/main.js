d979956aa4461c5766db9c8ecc22de1b2bbbccf6
spelmastaren <86412971+spelmastaren@users.noreply.github.com>
2026-03-19 18:19:36 +0100

    Försöker felhantera

--- Server/main.js
+++ Server/main.js
@@ -21,6 +21,7 @@ class lobby {
     GameUpdate(self) {
         // makes a list with all player names and positions
         let playerInfos = [];
+        console.log(self)
         for (const player of this.players) {
                 if (self.map != null && self.map[Math.floor(player.position.y + player.currentInput.y * PlayerSpeed)] != null && self.map[Math.floor(player.position.y + player.currentInput.y * PlayerSpeed)][Math.floor(player.position.x + player.currentInput.x * PlayerSpeed)] === 0) {
                     player.position.x += player.currentInput.x * PlayerSpeed;
