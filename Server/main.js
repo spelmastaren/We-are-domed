47b9849d713bd6494d88aa77c35d16f360579eb7
spelmastaren <86412971+spelmastaren@users.noreply.github.com>
2026-05-13 18:07:19 +0200

    Update main.js

--- Server/main.js
+++ Server/main.js
@@ -60,7 +60,7 @@ class lobby {
                         this.sholdChekIfendGame = true;
                     }
                     for (const enemy of this.enemies) { 
-                        if (Math.floor(enemy.position.x * 100) === Math.floor(player.position.x * 100) && Math.floor(enemy.position.y * 100) === Math.floor(player.position.y * 100)) {
+                        if (Math.floor(enemy.position.x * 400) === Math.floor(player.position.x * 400) && Math.floor(enemy.position.y * 400) === Math.floor(player.position.y * 400)) {
                             player.InGame = false;
                             console.log("Enemy has eaten player", player.Username, "in lobby with ID:", this.ID);
                             player.conection.send(JSON.stringify({ type: "Caught", data: {} }));
