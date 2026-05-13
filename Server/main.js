fa7069a88e83414c9d9c76c1a1510ceb9802333d
spelmastaren <86412971+spelmastaren@users.noreply.github.com>
2026-05-13 15:36:24 +0200

    Working mobs

--- Server/main.js
+++ Server/main.js
@@ -38,7 +38,7 @@ class lobby {
             while (true) {
                 const spawn = { x: Math.floor(Math.random() * 100), y: Math.floor(Math.random() * 100) };
                 if (this.map[Math.floor(spawn.y)][Math.floor(spawn.x)] === 0 && (spawn.x !== 10 || spawn.y !== 10)) {
-                    Enemy = new enemy(this, spawn);
+                    Enemy = new enemy(this, goalpos);
                     break;
                 }
             }
