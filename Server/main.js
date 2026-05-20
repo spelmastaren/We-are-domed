e0ad806f548997f2532f7d868886efcabd02a3ad
spelmastaren <86412971+spelmastaren@users.noreply.github.com>
2026-05-20 11:25:27 +0200

    Added kick perms to anti cheet

--- Server/main.js
+++ Server/main.js
@@ -265,10 +265,12 @@ function handelemessage(message,socket) {
     };
     if (messageJSON.type === "UpdateMovementInput") {
         if (Math.abs(messageJSON.data["x"]) >= 1 || Math.abs(messageJSON.data["y"]) >= 1) {
-            socket.send(JSON.stringify({ type: "error", data: { message: "Movement input out of bounds" } }));
+            socket.send(JSON.stringify({ type: "error", data: { message: "HAcking detected: Movement input out of bounds" } }));
+            socket.close();
             return;
         } else if (messageJSON.data["x"] === player.currentInput.x || messageJSON.data["y"] === player.currentInput.y) {
-            socket.send(JSON.stringify({ type: "error", data: { message: "Movement input unchanged are you hacking?" } }));
+            socket.send(JSON.stringify({ type: "error", data: { message: "Hacking Detected: Movement input unchanged are you hacking?" } }));
+            socket.close();
             return
         }
         player.currentInput.x = messageJSON.data["x"];
