db7c3d404e603dbf513f7766583587e0c7905f5b
spelmastaren <86412971+spelmastaren@users.noreply.github.com>
2026-06-02 16:53:53 +0200

    Update main.js

--- Server/main.js
+++ Server/main.js
@@ -642,9 +642,9 @@ wss.on("connection", (socket) => {
     // make a player object for them with name player and a number and connect player object to socket
     const playerObj = new player("Player " + playerjoinnumber,socket)
     // send that it was a success
-    playerObj.conection.send(JSON.stringify({ type: "Connection", data: { username: playerObj.username } }));
-    // bind it to the player map
     players.set(socket, playerObj);
+    // bind it to the player map
+    playerObj.conection.send(JSON.stringify({ type: "Connection", data: { username: playerObj.username } }));
     // add so next player gets one number higer
     playerjoinnumber++;
     // log the username
